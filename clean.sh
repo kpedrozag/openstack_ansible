e50662bdaebefdce75369b1d71aede0d97a1d3d0
Kevin Pedroza Goenaga <32544147+kpedrozag@users.noreply.github.com>
2018-12-04 14:06:21 -0500

    delete the image file

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 openstack image delete ubuntu_xenial
 rm /opt/stack/.ssh/my*
+rm /opt/stack/kevin/xenial-server-cloudimg-amd64-disk1.img
 openstack keypair delete mykey
 openstack server delete ubuntu_xenial_1
